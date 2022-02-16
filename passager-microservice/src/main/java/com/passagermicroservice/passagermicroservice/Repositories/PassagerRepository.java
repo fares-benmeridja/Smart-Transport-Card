0004a1100fbaea7871c4bab4c809c241d27fea28
fares Benmeridja <73068964+fares-benmeridja@users.noreply.github.com>
2022-02-16 09:29:55 +0100

    Update PassagerRepository.java

--- passager-microservice/src/main/java/com/passagermicroservice/passagermicroservice/Repositories/PassagerRepository.java
+++ passager-microservice/src/main/java/com/passagermicroservice/passagermicroservice/Repositories/PassagerRepository.java
@@ -3,5 +3,8 @@ package com.passagermicroservice.passagermicroservice.Repositories;
 import com.passagermicroservice.passagermicroservice.beans.Passager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface PassagerRepository extends JpaRepository<Passager, Integer> {
 }
